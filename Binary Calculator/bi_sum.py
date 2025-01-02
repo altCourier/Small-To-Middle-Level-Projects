05c2c5c965fd862927649f993c1790cfef62a1f6
altCourier <szehra_uluyol@outlook.com>
2025-01-02 19:58:55 +0300

    Add files via upload

---
+++ Binary Calculator/bi_sum.py
@@ -0,0 +1,18 @@
+def unpacker(num):
+    *num, = num # Unpack the given local variable.
+    return num[::-1]
+
+resulter = {"11": "1", "00": "0", "10": "1", "01": "1"}
+
+result = []
+
+num1 = unpacker("1001010111")
+num1 = unpacker("1001110101")
+
+if len(num1) == len(num2):
+    for index, bit in enumerate(zip(num1, num2)) # Created O(n) can be improved further.
+        
+        
+
+else:
+    pass
